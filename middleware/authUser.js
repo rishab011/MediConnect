7a59b1a1766fecd09f5251d3288b354bd4be17e0
rishab011 <146984092+rishab011@users.noreply.github.com>
2024-09-21 21:29:16 +0530

    Initial commit for MERN stack project

---
+++ middleware/authUser.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+const jwtSecret = process.env.JWT_SECRET;
+module.exports = function (req, res, next) {
+  // Get token from the header
+  const token = req.header("x-auth-token");
+
+  // Check if no token
+  if (!token) {
+    return res.status(401).json({ msg: "No token" });
+  }
+
+  // Verify token
+  try {
+    const decoded = jwt.verify(token, jwtSecret);
+
+    req.user = decoded.user;
+    next();
+  } catch (err) {
+    res.status(401).json({ msg: "token is not valid " });
+  }
+};
